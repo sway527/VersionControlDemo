42b77fed0c008d75b50a2f3129cb1598dbc66563
sway527 <21157@students.lynfield.school.nz>
2025-03-13 09:32:33 +1300

    Update GitHub - Step 2.py

--- GitHub - Step 2.py
+++ GitHub - Step 2.py
@@ -1,3 +1,6 @@
-# Version 2
-name = input("Enter your name:")
-print("Hello, {name}!")
+# Version 3
+while True:
+name = input(&quot;Enter your name (or &#39;exit&#39; to quit): &quot;)
+if name.lower() == &#39;exit&#39;:
+break
+print(f&quot;Hello, {name}!&quot;)
